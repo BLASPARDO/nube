1ed7e038e49406c1b27aae42c727eddcedf413e1
BLASPARDO <141207173+BLASPARDO@users.noreply.github.com>
2024-01-09 22:11:52 -0500

    ciencia.php

--- secciones/ciencia.php
+++ secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "CURA CANCER",
+"autor" => "OXANA BLAS",
+"resumen" => "Cáncer de mama, de colon y de próstata en fases precoces de diagnóstico (cada vez más habituales) alcanzan una curación del 90% y más",
 ];
 ?>
